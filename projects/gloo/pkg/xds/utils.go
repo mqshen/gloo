39dd721f3c588e61eb8a6935128ca4ba2ee91da3
ashleywang1 <ashley.wang@solo.io>
2021-01-20 19:06:06 -0500

    re-enable rest eds by default

--- projects/gloo/pkg/xds/utils.go
+++ projects/gloo/pkg/xds/utils.go
@@ -14,9 +14,13 @@ func SetEdsOnCluster(out *envoy_config_cluster_v3.Cluster, settings *v1.Settings
 	out.ClusterDiscoveryType = &envoy_config_cluster_v3.Cluster_Type{
 		Type: envoy_config_cluster_v3.Cluster_EDS,
 	}
-	// The default value for enableRestEds should be set to false via helm.
-	// If nil will not enable rest eds.
-	if settings.GetGloo().GetEnableRestEds().GetValue() {
+	// The default value for enableRestEds should be set to true via helm.
+	// If nil, will enable rest eds
+	restEds := true
+	if enableRestEds := settings.GetGloo().GetEnableRestEds(); enableRestEds != nil {
+		restEds = enableRestEds.GetValue()
+	}
+	if restEds {
 		out.EdsClusterConfig = &envoy_config_cluster_v3.Cluster_EdsClusterConfig{
 			EdsConfig: &envoy_config_core_v3.ConfigSource{
 				ResourceApiVersion: envoy_config_core_v3.ApiVersion_V3,
